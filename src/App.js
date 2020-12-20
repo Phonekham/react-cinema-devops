f2672a5c1c3ca48ae2fe0a7014e7301672f8d612
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-12-20 22:41:22 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import './App.scss';
+import Header from './components/header/Header';
+
 import store from './redux/store';
 
 function App() {
   return (
     <Provider store={store}>
+      <Header />
       <div className="App">redux</div>
     </Provider>
   );
