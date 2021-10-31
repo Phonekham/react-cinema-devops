3962173eb66ae81da312e0ac3f94a05077e6ef7d
Phonekham <phonekham.dev@gmail.com>
2021-10-31 10:17:11 +0700

    display image in grid

--- src/components/content/grid/Grid.jsx
+++ src/components/content/grid/Grid.jsx
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import PropTypes from "prop-types";
 
 import Rating from "../rating/Rating";
 import "./Grid.scss";
+import { IMAGE_URL } from "../../../services/movies.service";
 
 const Grid = (props) => {
-  const { images } = props;
+  const { list } = props;
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    setMovieData(list);
+  }, [list]);
+
   return (
     <>
       <div className="grid">
-        {images.map((image, i) => (
-          <div key={i}>
+        {movieData.map((data) => (
+          <div key={uuidv4()}>
             <div
               className="grid-cell"
-              style={{ backgroundImage: `url(${image.url})` }}
+              style={{
+                backgroundImage: `url(${IMAGE_URL}${data.poster_path})`,
+              }}
             >
               <div className="grid-read-more">
                 <button className="grid-cell-button">Read More</button>
               </div>
               <div className="grid-detail">
-                <span className="grid-detail-title">Mission Impossible</span>
+                <span className="grid-detail-title">{data.title}</span>
                 <div className="grid-detail-rating">
-                  <Rating rating={image.rating} totalStars={10} />
+                  <Rating rating={data.vote_average} totalStars={10} />
                   &nbsp;&nbsp;
-                  <div className="grid-vote-average">{image.rating}</div>
+                  <div className="grid-vote-average">{data.vote_average}</div>
                 </div>
               </div>
             </div>
@@ -33,4 +45,12 @@ const Grid = (props) => {
   );
 };
 
-export default Grid;
+Grid.propTypes = {
+  list: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  list: state.movies.list,
+});
+
+export default connect(mapStateToProps, {})(Grid);
