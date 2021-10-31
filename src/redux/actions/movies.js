561ce36891e8c5982c6055d9d0615a023deb8eec
Phonekham <phonekham.dev@gmail.com>
2021-10-31 10:32:25 +0700

    load movies in redux action

--- src/redux/actions/movies.js
+++ src/redux/actions/movies.js
@@ -9,8 +9,8 @@ import { MOVIE_API_URL } from "../../services/movies.service";
 
 export const getMovies = (type, pageNumber) => async (dispatch) => {
   try {
-    const response = await MOVIE_API_URL(type, pageNumber);
-    const { results, payload } = response.data;
+    const response = await getMoviesRequest(type, pageNumber);
+    const { results, payload } = response;
     dispatchMethod(MOVIE_LIST, results, dispatch);
     dispatchMethod(RESPONSE_PAGE, payload, dispatch);
   } catch (error) {
@@ -22,4 +22,30 @@ export const getMovies = (type, pageNumber) => async (dispatch) => {
 
 const dispatchMethod = (type, payload, dispatch) => {
   dispatch({ type, payload });
+};
+
+export const loadMoreMovies = (type, pageNumber) => async (dispatch) => {
+  try {
+    const response = await getMoviesRequest(type, pageNumber);
+    const { results, payload } = response;
+    dispatchMethod(
+      LOAD_MORE_RESULTS,
+      { list: results, page: payload.page, totalPages: payload.totalPages },
+      dispatch
+    );
+  } catch (error) {
+    if (error.response) {
+      dispatchMethod(SET_ERROR, error.response.data.message, dispatch);
+    }
+  }
+};
+
+const getMoviesRequest = async (type, pageNumber) => {
+  const movies = await MOVIE_API_URL(type, pageNumber);
+  const { results, page, total_pages } = movies.data;
+  const payload = {
+    page,
+    totalPages: total_pages,
+  };
+  return { results, payload };
 };
